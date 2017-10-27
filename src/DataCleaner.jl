7b34bcdb16e580c56977541bc62f3670c569ad5d
Yao Lu <luyaocns@gmail.com>
2017-10-27 13:37:41 +0800

    add precompile

--- src/DataCleaner.jl
+++ src/DataCleaner.jl
@@ -1,12 +1,15 @@
+__precompile__(true)
+
 module DataCleaner
 
 using DataFrames
 
-export iconv, iconv!
-iconv(fn, from = "GBK", to = "UTF-8") = readstring(`iconv -f GBK -t UTF-8 $fn`)
+export iconv, iconv!, readtable_cn, fillna, fillna!, naplot
+
+iconv(fn, from = "GBK", to = "UTF-8") = readstring(`iconv -f $from -t $to $fn`)
+
 iconv!(fn, from = "GBK", to = "UTF-8") = write(fn, iconv(fn, from, to))
 
-export readtable_cn
 function readtable_cn(fn, args...; kwargs...)
   fn_utf = tempname()
   write(fn_utf, replace(iconv(fn), r"([^,])(?=\r\n)", s"\1,"))
@@ -14,11 +17,13 @@ function readtable_cn(fn, args...; kwargs...)
 end
 
 DataFrames.isna(x::Real) = isnan(x)
+
 DataFrames.isna(x::AbstractArray{Real}) = isnan.(x)
 
-export fillna, fillna!
 fillna(x) = fillna!(copy(x))
+
 fillna!(df::DataFrame) = map(fillna!, df.columns)
+
 function fillna!(x::AbstractArray)
   for i in eachindex(x)[2:end]
     isna(x[i]) && (x[i] = x[i - 1])
@@ -28,6 +33,7 @@ function fillna!(x::AbstractArray)
   end
   x
 end
+
 function fillna!(x::AbstractArray, y::AbstractArray, f::Function)
   for i in eachindex(x)
     isna(x[i]) && (x[i] = f(y[i]))
@@ -35,9 +41,10 @@ function fillna!(x::AbstractArray, y::AbstractArray, f::Function)
   x
 end
 
-export naplot
 naplot(x::AbstractVector) = Main.plot(isna(x))
+
 naplot(x::AbstractMatrix) = Main.heatmap(isna(x))
+
 function naplot(df::DataFrame)
   x = 1:size(df, 2) # string.(df.colindex.names)
   y = 1:size(df, 1)
